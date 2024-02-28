e592dbeff4d8f718666a18d55e3bb64457dd5962
Roman Kravchenko <4job00@mail.ru>
2024-02-28 20:32:05 +0300

    удаление папок

--- Freelancing/Palindrome Number.py
+++
@@ -1,5 +0,0 @@
-"""Task"""
-"""Given an integer x, return true if x is a palindrome, and false otherwise."""
-class Solution:
-    def isPalindrome(self, x: int) -> bool:
-        return False if x < 0 else x == int(str(x)[::-1])

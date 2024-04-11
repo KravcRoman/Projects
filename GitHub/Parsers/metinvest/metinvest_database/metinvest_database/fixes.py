dcb504a2010f4e20ec9c91473c94c80018af0d32
KravcRoman <130038762+KravcRoman@users.noreply.github.com>
2024-04-11 19:46:56 +0300

    Delete GitHub/Parsers/metinvest directory

--- GitHub/Parsers/metinvest/metinvest_database/metinvest_database/fixes.py
+++
@@ -1,8 +0,0 @@
-from metinvest_database.shared_models import City, SupplierCity
-
-def fix_city_parser():
-  for city in City.objects.all():
-    supplier_city = SupplierCity.objects.filter(city=city)
-    if supplier_city:
-      city.parser = supplier_city[0].supplier.parser
-      city.save()

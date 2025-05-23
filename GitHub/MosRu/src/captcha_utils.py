d8038f6506af59cd54ce31424ad7556f76b73321
Roman Kravchenko <130038762+KravcRoman@users.noreply.github.com>
2025-05-23 17:36:29 +0300

    Update captcha_utils.py

--- GitHub/MosRu/src/captcha_utils.py
+++ GitHub/MosRu/src/captcha_utils.py
@@ -1,5 +1,3 @@
-# captcha_utils.py
-
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
